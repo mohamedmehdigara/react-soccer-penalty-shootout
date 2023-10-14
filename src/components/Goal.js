cb8147710531661cb718cb93bd309945a532bc0f
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-14 17:09:09 +0100

    edited files

--- src/components/Goal.js
+++ src/components/Goal.js
@@ -6,24 +6,43 @@ const GoalContainer = styled.div`
   position: relative;
   width: 200px;
   height: 100px;
-  background-color: #00a0e3;
+  background: linear-gradient(90deg, transparent 40%, #00a0e3 40%, #00a0e3 60%, transparent 60%);
+  background-size: 20px 20px;
   margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
 `;
 
 const GoalPost = styled.div`
   position: absolute;
-  width: 10px;
+  width: 20px; /* Thicker goal posts */
   height: 100px;
   background-color: #fff;
   top: 0;
   left: 0;
   right: 0;
   margin: 0 auto;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+`;
+
+const Crossbar = styled.div`
+  position: absolute;
+  width: 200px; /* Wider crossbar */
+  height: 10px; /* Thicker crossbar */
+  background-color: #fff;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
 `;
 
 const Goal = ({ goalHeight }) => (
   <GoalContainer>
     <GoalPost style={{ height: goalHeight + 'px' }} />
+    <Crossbar />
   </GoalContainer>
 );
 
--- src/components/Goalkeeper.js
+++ src/components/Goalkeeper.js
@@ -1,16 +1,70 @@
 // src/components/Goalkeeper.js
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const GoalkeeperContainer = styled.div`
   position: relative;
   width: 100px;
-  height: 100px;
+  height: 200px; /* Make the goalkeeper taller */
   background-color: #ff5722;
+  display: flex;
+  align-items: flex-end;
+`;
+
+const GoalkeeperBody = styled.div`
+  position: relative;
+  width: 100%;
+  height: 70%;
+  background-color: #ff5722;
+  border-radius: 50% 50% 0 0;
+  border: 1px solid #fff;
+  box-shadow: 0 10px 15px rgba(0, 0, 0, 0.3); /* Add a shadow for depth */
+`;
+
+const GoalkeeperArms = styled.div`
+  position: absolute;
+  width: 10px;
+  height: 60%;
+  background-color: #ff5722;
+  border-radius: 0 0 50% 50%;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: 0 auto;
+  box-shadow: 0 10px 15px rgba(0, 0, 0, 0.3); /* Add a shadow for depth */
+`;
+
+const GoalkeeperLegs = styled.div`
+  position: absolute;
+  width: 20px;
+  height: 100px; /* Make the legs longer */
+  background-color: #ff5722;
+  border-radius: 50% 50% 0 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: 0 auto;
+  box-shadow: 0 10px 15px rgba(0, 0, 0, 0.3); /* Add a shadow for depth */
+`;
+
+const goalkeeperAnimation = keyframes`
+  0% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(-20px); /* Simulate goalkeeper movement */
+  }
+  100% {
+    transform: translateX(0);
+  }
 `;
 
 const Goalkeeper = ({ position }) => (
-  <GoalkeeperContainer style={{ left: `${position}px` }} />
+  <GoalkeeperContainer style={{ left: `${position}px` }}>
+    <GoalkeeperBody />
+    <GoalkeeperArms />
+    <GoalkeeperLegs />
+  </GoalkeeperContainer>
 );
 
 export default Goalkeeper;
